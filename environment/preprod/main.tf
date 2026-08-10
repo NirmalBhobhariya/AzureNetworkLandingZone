90619ce89bb91cbc13add59922884c0cf01810cb
Nirmal Bhobhariya <nirmalbhobhariya01@gmail.com>
2026-08-10 18:01:47 +0530

    fix terraform formatting

--- environment/preprod/main.tf
+++ environment/preprod/main.tf
@@ -1,19 +1,19 @@
 module "resource_group" {
   source = "../../modules/azurerm_resource_group"
-  rg = var.rg
+  rg     = var.rg
 }
 module "virtual_network" {
-    depends_on = [ module.resource_group ]
-  source = "../../modules/azurerm_virtual_network"
-  vnet = var.vnet
+  depends_on = [module.resource_group]
+  source     = "../../modules/azurerm_virtual_network"
+  vnet       = var.vnet
 }
 module "subnet" {
-    depends_on = [ module.virtual_network ]
-  source = "../../modules/azurerm_subnet"
-  snet = var.snet
+  depends_on = [module.virtual_network]
+  source     = "../../modules/azurerm_subnet"
+  snet       = var.snet
 }
-module "network_security_group" { 
-    depends_on = [ module.resource_group ]
-  source = "../../modules/azurerm_network_security_group"
-  nsg = var.nsg
+module "network_security_group" {
+  depends_on = [module.resource_group]
+  source     = "../../modules/azurerm_network_security_group"
+  nsg        = var.nsg
 }
